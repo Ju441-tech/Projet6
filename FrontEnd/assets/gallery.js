58eeee1999fb97da543051dce3eca828662450f4
Ju441-tech <merlin441977@gmail.com>
2023-06-14 22:36:11 +0200

    First commit

---
+++ FrontEnd/assets/gallery.js
@@ -0,0 +1,15 @@
+//Ici, on créer le JS pour l'affichage dynamique de la gallery dans la page web au lieu de l'écrire dans le Html
+const gallery=document.querySelector(".gallery");
+for(let i=0;i<10;i++){
+const articleElement=document.createElement("article");
+
+
+gallery.appendChild(articleElement);
+articleElement.innerText="Projet"+(i+1);
+};
+//Ajout eventListners aux boutons filtres
+//Bouton tous OUAI!!!!MAIS SI JE N'AI PAS DE LISTE DES PROJETS, JE NE PEUX PAS FAIRE CA !!!! :
+const boutonTous=document.querySelector("#tous");
+boutonTous.addEventListener("click",function(){
+    //gallery.innerHTML=""; //Efface toute la partie gallery
+})
